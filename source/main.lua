4e4d86f1895913930aa57e21be3b1a0f66578e2d
marcupoilu <castejon.morgan@gmail.com>
2024-07-27 21:00:24 +0200

    object structure

--- source/main.lua
+++ source/main.lua
@@ -1,85 +1,24 @@
-
-
--- Name this file `main.lua`. Your game can use multiple source files if you wish
--- (use the `import "myFilename"` command), but the simplest games can be written
--- with just `main.lua`.
-
--- You'll want to import these in just about every project you'll work on.
+gfx = playdate.graphics
 
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+import "player.lua"
 
--- Declaring this "gfx" shorthand will make your life easier. Instead of having
--- to preface all graphics calls with "playdate.graphics", just use "gfx."
--- Performance will be slightly enhanced, too.
--- NOTE: Because it's local, you'll have to do it in every .lua source file.
 
-local gfx <const> = playdate.graphics
-
--- Here's our player sprite declaration. We'll scope it to this file because
--- several functions need to access it.
-
-local playerSprite = nil
-
--- A function to set up our game environment.
-
-function myGameSetUp()
-
-    -- Set up the player sprite.
-    
-    local cannonBase = gfx.image.new("cannon_base")
-    local cannonGun = gfx.image.new("cannon_gun")
-    assert( cannonBase ) -- make sure the image was where we thought
-    assert( cannonGun ) -- make sure the image was where we thought
-
-	
-    cannonBaseSprite = gfx.sprite.new( cannonBase )
-    cannonGunSprite = gfx.sprite.new( cannonGun )
-
-    --cannonGunSprite:setOpaque(true)
-
-    cannonGunSprite:setCenter(0.5, 1)
-    cannonBaseSprite:moveTo( 200, 220 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
-
-    cannonGunSprite:moveTo( 200, 210 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
-    cannonBaseSprite:add() -- This is critical!
-    cannonGunSprite:add() -- This is critical!
-
-    -- We want an environment displayed behind our sprite.
-    -- There are generally two ways to do this:
-    -- 1) Use setBackgroundDrawingCallback() to draw a background image. (This isB what we're doing below.)
-    -- 2) Use a tilemap, assign it to a sprite with sprite:setTilemap(tilemap),
-    --       and call :setZIndex() with some low number so the background stays behind
-    --       your other sprites.
-
-end
-
--- Now we'll call the function above to configure our game.
--- After this runs (it just runs once), nearly everything will be
--- controlled by the OS calling `playdate.update()` 30 times a second.
-
-myGameSetUp()
-
--- `playdate.update()` is the heart of every Playdate game.
--- This function is called right before every frame is drawn onscreen.
--- Use this function to poll input, run game logic, and move sprites.
+local player = Player:new();
+local player2 = Player:new();
+local player3 = Player:new();
+player:init({x=200,y=220}, {x=200,y=210});
+-- player2:Init( {x=50,y=220}, {x=50,y=210})
+-- player3:Init({x=350,y=220}, {x=350,y=210})
 
 function playdate.update()
 
-    -- Poll the d-pad and move our player accordingly.
-    -- (There are multiple ways to read the d-pad; this is the simplest.)
-    -- Note that it is possible for more than one of these directions
-    -- to be pressed at once, if the user is pressing diagonally.
+    crankPosition = playdate.getCrankPosition()
 
-    local crankPosition = playdate.getCrankPosition()
-
-    cannonGunSprite:setRotation(crankPosition)
-
-    -- Call the functions below in playdate.update() to draw sprites and keep
-    -- timers updated. (We aren't using timers in this example, but in most
-    -- average-complexity games, you will.)
+    player:update()
 
     gfx.sprite.update()
     playdate.timer.updateTimers()
---
+++ source/player.lua
@@ -0,0 +1,31 @@
+class("Player").extends()
+
+function Player:new(o)
+    o = o or {}
+    setmetatable(o,self)
+    self.__index = self
+    return o
+end
+
+function Player:init( basePosition, gunPosition)
+    
+    local cannonBase = gfx.image.new("images/cannon_base")
+    local cannonGun = gfx.image.new("images/cannon_gun")
+    assert( cannonBase ) -- make sure <he image was where we thought
+    assert( cannonGun ) -- make sure the image was where we thought
+
+    self.cannonBaseSprite = gfx.sprite.new( cannonBase )
+    self.cannonGunSprite = gfx.sprite.new( cannonGun )
+
+    self.cannonGunSprite:setCenter(0.5, 1)
+    self.cannonBaseSprite:moveTo( basePosition.x, basePosition.y ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
+    print(gunPosition.x)
+    self.cannonGunSprite:moveTo( gunPosition.x, gunPosition.y ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
+    self.cannonBaseSprite:add() -- This is critical!
+    self.cannonGunSprite:add() -- This is critical!
+
+end
+
+function Player:update()
+    self.cannonGunSprite:setRotation(crankPosition)
+end
