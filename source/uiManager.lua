8437aafe0141d37a5945b7dddd3b2374b0fd7294
Dayuki <acastejon.pro@gmail.com>
2024-08-14 18:32:07 +0200

    Commented HP bar behaviour

--- source/uiManager.lua
+++ source/uiManager.lua
@@ -16,8 +16,8 @@ function UiManager:init()
     self.horizontalLayoutHPCell = HorizontalLayout(22, -4, player.hpMax, 200)
     self.horizontalLayoutHPFillCell = HorizontalLayout(16, 2, player.hp, 203)
     self.horizontalLayoutLevelUp = HorizontalLayout(levelUpCellWidth, levelUpDistance, levelUpCellNumber, 20)
-    -- hpCells = self:createHPBar(hpCells, "images/hpbar_cell", self.horizontalLayoutHPCell.positionBaseX + 10, self.horizontalLayoutHPCell.positionBaseY, self.horizontalLayoutHPCell.cellSize, self.horizontalLayoutHPCell.distance, 0.85)
-    -- hpFillCells = self:createHPBar(hpFillCells, "images/hpbar_cellfill", self.horizontalLayoutHPFillCell.positionBaseX + 7, self.horizontalLayoutHPFillCell.positionBaseY, self.horizontalLayoutHPFillCell.cellSize, self.horizontalLayoutHPFillCell.distance, 0.85)
+    hpCells = self:createHPBar(hpCells, "images/hpbar_cell", self.horizontalLayoutHPCell.positionBaseX + 10, self.horizontalLayoutHPCell.positionBaseY, self.horizontalLayoutHPCell.cellSize, self.horizontalLayoutHPCell.distance, 0.85)
+    hpFillCells = self:createHPBar(hpFillCells, "images/hpbar_cellfill", self.horizontalLayoutHPFillCell.positionBaseX + 7, self.horizontalLayoutHPFillCell.positionBaseY, self.horizontalLayoutHPFillCell.cellSize, self.horizontalLayoutHPFillCell.distance, 0.85)
 end
 
 function UiManager:generateUpgrades()
@@ -66,9 +66,9 @@ function UiManager:levelUpDisplay()
         generate = false
         ups[levelUpIndex +1]:applyUpgrade()
         playdate.update = gameUpdate
-        -- hpCells = self:createHPBar(hpCells, "images/hpbar_cell", self.horizontalLayoutHPCell.positionBaseX + 10, self.horizontalLayoutHPCell.positionBaseY, self.horizontalLayoutHPCell.cellSize, self.horizontalLayoutHPCell.distance, 0.85)
-        -- hpFillCells = self:createHPBar(hpFillCells, "images/hpbar_cellfill", self.horizontalLayoutHPFillCell.positionBaseX + 7, self.horizontalLayoutHPFillCell.positionBaseY, self.horizontalLayoutHPFillCell.cellSize, self.horizontalLayoutHPFillCell.distance, 0.85)
-        -- self:updateHPDisplay()
+        hpCells = self:createHPBar(hpCells, "images/hpbar_cell", self.horizontalLayoutHPCell.positionBaseX + 10, self.horizontalLayoutHPCell.positionBaseY, self.horizontalLayoutHPCell.cellSize, self.horizontalLayoutHPCell.distance, 0.85)
+        hpFillCells = self:createHPBar(hpFillCells, "images/hpbar_cellfill", self.horizontalLayoutHPFillCell.positionBaseX + 7, self.horizontalLayoutHPFillCell.positionBaseY, self.horizontalLayoutHPFillCell.cellSize, self.horizontalLayoutHPFillCell.distance, 0.85)
+        self:updateHPDisplay()
     end
     if playdate.buttonJustPressed(playdate.kButtonB) then
         self:generateUpgrades()
@@ -76,27 +76,27 @@ function UiManager:levelUpDisplay()
 end
 
 function UiManager:updateHPDisplay()
-    for i = 1, player.hpMax do
-        if(i <= player.hp) then
-            hpFillCells[i]:setVisible(true)
-        else
-            hpFillCells[i]:setVisible(false)
-        end
-    end
+    -- for i = 1, player.hpMax do
+    --     if(i <= player.hp) then
+    --         hpFillCells[i]:setVisible(true)
+    --     else
+    --         hpFillCells[i]:setVisible(false)
+    --     end
+    -- end
 end
 
 function UiManager:createHPBar(tableToClear, image, positionBaseX, positionBaseY, cellSize, marge, scale)
-    for key, value in pairs(tableToClear) do
-        value:remove()
-    end
-    tableToClear = {}
-    local offset = 0
-    for i = 0, player.hpMax -1 do
-        local hpCell = UISprite(image, 2, 0.5, 0, positionBaseX + offset, positionBaseY)
-        hpCell:setScale(scale)
-        hpCell:moveTo(positionBaseX + (i*(cellSize+marge)), positionBaseY)
-        table.insert(tableToClear,hpCell)
-    end
-    return tableToClear
+    -- for key, value in pairs(tableToClear) do
+    --     value:remove()
+    -- end
+    -- tableToClear = {}
+    -- local offset = 0
+    -- for i = 0, player.hpMax -1 do
+    --     local hpCell = UISprite(image, 2, 0.5, 0, positionBaseX + offset, positionBaseY)
+    --     hpCell:setScale(scale)
+    --     hpCell:moveTo(positionBaseX + (i*(cellSize+marge)), positionBaseY)
+    --     table.insert(tableToClear,hpCell)
+    -- end
+    -- return tableToClear
 
 end
