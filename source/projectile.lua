6c54eb75046aa5b91d91105580e5bc2f28302273
Dayuki <acastejon.pro@gmail.com>
2024-10-28 18:05:33 +0100

    Update projectile.lua

--- source/projectile.lua
+++ source/projectile.lua
@@ -38,7 +38,6 @@ function Projectile:update()
     if self.duration == nil or self.duration == 0 then
         return
     end
-    print(1/playdate.display.getRefreshRate() * 1000)
     self.duration -= 1/playdate.display.getRefreshRate() * 1000
     if self.duration <= 0 then
         self:destroy()
