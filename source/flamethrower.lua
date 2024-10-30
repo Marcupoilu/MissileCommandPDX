630a662bdcb06e060ac68842a946124fa0daa3e3
Dayuki <acastejon.pro@gmail.com>
2024-10-30 12:44:27 +0100

    Update flamethrower.lua

--- source/flamethrower.lua
+++ source/flamethrower.lua
@@ -7,7 +7,6 @@ function Flamethrower:init(attackSpeed, x, y, speed, damage, projectileAmount, s
     self.x = x
     self.duration = duration
     self.y = y
-    self.projectileAmount += 1
     -- self:debugLevel(10)
 end
 
