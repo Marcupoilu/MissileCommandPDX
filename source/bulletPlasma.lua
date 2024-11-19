870e9ba1029442257c83b12e26b5835c543db0c8
Ikimasho_Morgan <morgan.castejon@ikimasho.games>
2024-11-19 15:43:49 +0100

    [Bullet plasma fixed]

--- source/bulletPlasma.lua
+++ source/bulletPlasma.lua
@@ -14,16 +14,23 @@ end
 function BulletPlasma:update()
     BulletPlasma.super.update(self)
     self:animate()
-    self.radius += self.speed + (((player.projectileSpeedBonus*self.speed)/100)) * deltaTime
-    self:moveTo(self.radius*math.cos(math.rad(self.originAngle + self.offset)) + self.originPosition.x, self.radius*math.sin(math.rad(self.originAngle + self.offset)) + self.originPosition.y)
-    -- self.rot += self.speed * 10
-    -- self:setRotation(self.rot)
+    
+    -- Mise à jour de la position en fonction de l'angle et de l'offset
+    self.radius += self.speed + (((player.projectileSpeedBonus * self.speed) / 100)) * deltaTime
+    self:moveTo(
+        self.radius * math.cos(math.rad(self.originAngle + self.offset)) + self.originPosition.x,
+        self.radius * math.sin(math.rad(self.originAngle + self.offset)) + self.originPosition.y
+    )
+
+    -- Gestion des rebonds sur les bords
     if self.x <= 0 or self.x >= 400 then
-        self.originAngle = 180-self.originAngle
+        self.originAngle = 180 - self.originAngle  -- Inversion horizontale
+        self.offset = -self.offset                -- Appliquer l'inversion à l'offset
         self:resetPosAndRot()
     end
     if self.y <= 0 or self.y >= 240 then
-        self.originAngle =  -self.originAngle
+        self.originAngle = -self.originAngle      -- Inversion verticale
+        self.offset = -self.offset                -- Appliquer l'inversion à l'offset
         self:resetPosAndRot()
     end
 end
