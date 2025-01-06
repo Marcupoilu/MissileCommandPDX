53bf531f3c54f478c595b8541cba5f50e822339d
Ikimasho_Morgan <morgan.castejon@ikimasho.games>
2025-01-06 17:59:51 +0100

    [Toxic Vape fixed]

--- source/toxicVape.lua
+++ source/toxicVape.lua
@@ -66,5 +66,5 @@ end
 function ToxicVape:shoot()
     ToxicVape.super.shoot()
     local angle = 0
-    bulletToxicVape = BulletToxicVape(self.x, self.y - 20, self.speed, self.damage, angle, self.scale, self.duration, self.tickNumber, self.spread, self.tickTime self.tick)
+    bulletToxicVape = BulletToxicVape(self.x, self.y - 20, self.speed, self.damage, angle, self.scale, self.duration, self.tickNumber, self.spread, self.tickTime, self.tick)
 end
