c50db90d4824da281a948d59915a3759b9de70e8
Dayuki <acastejon.pro@gmail.com>
2025-01-06 18:04:21 +0100

    Balancing Aura

--- source/aura.lua
+++ source/aura.lua
@@ -7,6 +7,7 @@ function Aura:init(attackSpeed, x, y, speed, damage, projectileAmount, scale, du
     self.x = x
     self.duration = duration
     self.y = y
+    self.tick = 600
     -- self:debugLevel(10)
 end
 
@@ -14,42 +15,40 @@ function Aura:changeLevel()
     self.level += 1
     if self.level == 2 then
         self.scale += 0.1
-        self.damage += 0.1
     end
     if self.level == 3 then
         self.scale += 0.1
-        self.damage += 0.1
+        self.damage += 0.05
     end
     if self.level == 4 then
         self.scale += 0.1
-        self.damage += 0.1
     end
     if self.level == 5 then
-        self.scale += 0.1
+        self.scale += 0.25
         self.damage += 0.1
+        self.tick -= 150
     end
     if self.level == 6 then
         self.scale += 0.1
-        self.damage += 0.1
     end
     if self.level == 7 then
         self.scale += 0.1
-        self.damage += 0.1
+        self.damage += 0.05
     end
     if self.level == 8 then
         self.scale += 0.1
-        self.damage += 0.1
     end
     if self.level == 9 then
         self.scale += 0.1
-        self.damage += 0.1
+        self.damage += 0.05
     end
     if self.level == 10 then
-        self.scale += 0.2
-        self.damage += 0.2
+        self.scale += 0.25
+        self.damage += 0.25
+        self.tick -= 150
     end
 end
 
 function Aura:shoot()
-    bulletAura = BulletAura(self.x, self.y, self.speed, self.damage, angle, self.scale, self.duration)
+    bulletAura = BulletAura(self.x, self.y, self.speed, self.damage, angle, self.scale, self.duration, self.tick)
 end
