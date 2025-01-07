7a5dd3da7c3ad6fc1985b8726fee4abb8d8a2ccc
Dayuki <acastejon.pro@gmail.com>
2025-01-07 17:09:48 +0100

    SimpleCannon, Shotgun and Serpentine reworked

--- source/simpleCannon.lua
+++ source/simpleCannon.lua
@@ -11,46 +11,22 @@ end
 function SimpleCannon:changeLevel()
     self.level += 1
     if self.level == 2 then
-        self.damage += 0.1
-        self.attackSpeed -= 25
-        self.scale += 0.1
+        self.damage += 0.5
+        self.scale += 0.25
     end
     if self.level == 3 then
-        self.speed += 0.25
+        self.projectileAmount += 1
+        self.speed += 2
     end
     if self.level == 4 then
-        self.attackSpeed -= 50
-        self.scale += 0.1
+        self.attackSpeed -= 100
+        self.scale += 0.25
+        self.speed += 3
     end
     if self.level == 5 then
         self.projectileAmount += 1
-        self.damage += 0.1
-    end
-    if self.level == 6 then
-        self.attackSpeed -= 25
-        self.scale += 0.1
-        self.speed += 0.25
-    end
-    if self.level == 7 then
-        self.damage += 0.15
-    end
-    if self.level == 8 then
-        self.attackSpeed -= 50
-        self.scale += 0.1
-        self.speed += 0.5
-    end
-    if self.level == 9 then
-        self.damage += 0.15
-        self.scale += 0.2
-        self.speed += 0.5
-
-    end
-    if self.level == 10 then
-        self.projectileAmount += 1
         self.damage += 0.5
-        self.attackSpeed -= 50
-        self.scale += 0.5
-        self.speed += 1
+        self.attackSpeed -= 100
     end
 end
 
