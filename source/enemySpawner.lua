554261ed3d74d9c3d9d4dc5cd581e43b274b4fe9
Ikimasho_Morgan <morgan.castejon@ikimasho.games>
2025-01-30 15:08:50 +0100

    [Pool enemies push back]

--- source/enemySpawner.lua
+++ source/enemySpawner.lua
@@ -13,9 +13,9 @@ function EnemySpawner:spawn()
     local enemy = BulletPool:get(self.enemy)
     if enemy then
         enemy:reset(0, self.y - enemy:getSize(), self.enemyData.speed, self.enemyData.hp, self.enemyData.xp, self.enemyData.damage, self.enemyData.enemyImage, self.enemyData.core)
-    -- else
-    --     enemy = self.enemy(0, self.y, self.enemyData.speed, self.enemyData.hp, self.enemyData.xp, self.enemyData.damage, self.enemyData.enemyImage, self.enemyData.core)
-    --     BulletPool:release(enemy) -- On l'ajoute au pool pour la prochaine fois
+    else
+        enemy = self.enemy(0, self.y, self.enemyData.speed, self.enemyData.hp, self.enemyData.xp, self.enemyData.damage, self.enemyData.enemyImage, self.enemyData.core)
+        BulletPool:release(enemy) -- On l'ajoute au pool pour la prochaine fois
     end 
     EnemySpawner.super.spawn(self, enemy)
     self.enemy.spawner = self
